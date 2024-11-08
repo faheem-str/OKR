793b5a9cdbaecbfb0f5fb61afaa8da278e005421
syedfaheem <syedfaheem@xerago.com>
2024-11-08 12:35:41 +0530

    dashboard sets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import LoginPage from './component/LoginPage/LoginPage'
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-
+import Dashboard from './component/Dashboard/Dashboard';
 function App() {
   return (
     <div className="App">
        <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        {/* <Route path="/home" element={<HomePage />} /> */}
+        <Route path="/home" element={<Dashboard />} />
         {/* Redirect to login if no route matches */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
